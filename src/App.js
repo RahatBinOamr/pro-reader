dabea54ba465a511196e612cb63e601c7d60bae7
RahatBinOamr <108364543+RahatBinOamr@users.noreply.github.com>
2023-01-15 10:15:03 +0300

    change

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from "react";
+import { RouterProvider } from "react-router-dom";
+import router from "./Router/Router";
+
 
 function App() {
-  return <h1> Welcome to proReader</h1>
+  
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -1,12 +1,25 @@
 import React from 'react'
-import Header from './Header'
+import { Link, useRouteError } from 'react-router-dom'
+
 
 const ErrorPage = () => {
+  const error = useRouteError()
   return (
     <>
-      <Header />
+    
       <div className='flex flex-col min-h-[700px] justify-center items-center'>
-        <h1 className='text-4xl'>Ops! An Error Ocurred!</h1>
+        <h1 className='text-4xl'>Ops! An Error Ocurred!</h1><br />
+        {
+          error && (
+           <div>
+            <p className='text-red-600 text-xl'> {error.statusText || error.message} </p> <br />
+            <p className='text-3xl text-gray-500'>{error.status} </p> <br />
+          <Link to={'/'}><button className="btn btn-success">Go To Home</button></Link>
+          
+           </div>
+
+          )
+        }
       </div>
     </>
   )
